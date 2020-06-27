b7db4a3830f4b605f43aa507f352d4bf9b56da1a
Tianze Li <tianze.li.eu@gmail.com>
2020-06-27 13:42:35 +0200

    Add RPY for IMU for reference.

--- am_sensors/src/imu_trans.cpp
+++ am_sensors/src/imu_trans.cpp
@@ -1,7 +1,9 @@
 #include <ros/ros.h>
+#include <tf/tf.h>
 #include <sensor_msgs/Imu.h>
 #include <std_msgs/String.h>
 #include <sstream>
+#include "geometry_msgs/Vector3.h"
 
 
 class imu_sub_pub
@@ -16,8 +18,9 @@ public:
 		ss_tmp << topic_name << "/processed";
 		imu_new_topic = ss_tmp.str();
 
-		imu_pub_= nh_.advertise<sensor_msgs::Imu>(imu_new_topic, 20);
+		imu_pub_= nh_.advertise<sensor_msgs::Imu>(imu_new_topic, 200);
 		imu_sub_ = nh_.subscribe(topic_name, 10, &imu_sub_pub::imu_callback, this);
+		imu_pub_rpy_ = nh_.advertise<geometry_msgs::Vector3>("rpy", 200);
 	}
 
 	void imu_callback(const sensor_msgs::Imu &msg)
@@ -25,12 +28,24 @@ public:
 		sensor_msgs::Imu imu_tmp = msg;
 		imu_tmp.header.frame_id = frame_name;
 		imu_pub_.publish(imu_tmp);
+
+		tf::Quaternion q(
+        	msg.orientation.x,
+        	msg.orientation.y,
+        	msg.orientation.z,
+        	msg.orientation.w);
+    	tf::Matrix3x3 m(q);
+		geometry_msgs::Vector3 rpy;
+    	m.getRPY(rpy.x, rpy.y, rpy.z);
+    	imu_pub_rpy_.publish(rpy);
+
 	}
 
 private:
   ros::NodeHandle nh_; 
   ros::Publisher imu_pub_;
   ros::Subscriber imu_sub_;
+  ros::Publisher imu_pub_rpy_;
 
   std::string topic_name = "notgiven";
   std::string imu_new_topic = "notgiven";
