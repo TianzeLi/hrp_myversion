5f173081d20dd6176692c0be7ca9b9f68344f4e9
Tianze Li <tianze.li.eu@gmail.com>
2020-08-17 17:10:25 +0200

    Modify the tracker into object to allow multiple instances.

--- am_sensors/src/tracking_marker_pub.cpp
+++ am_sensors/src/tracking_marker_pub.cpp
@@ -1,62 +1,139 @@
+/**
+ * Subscibes to the pose topics and publishes the trace in rviz,
+ * in sequentially red, orange, yellow, green, cyan, blue, purple.
+ * 
+ * By default, the trackers are published in /map frame,
+ * and pose are in nav_msgs::Odometry
+ * 
+ * TODO: add covariance ellipse? 
+ */
+
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
-#include<nav_msgs/Odometry.h>
-#include<geometry_msgs/Point.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/Vector3.h>
 
 
-#define MAP_FRAME_NAME 	"/map"
-#define POSE_TOPIC 		"/odometry/filtered"
-#define POSE_TYPE 		nav_msgs::Odometry 
-POSE_TYPE pose_tmp;
-
-
-void pose_callback(const POSE_TYPE &msg)
+class tracker
 {
-	pose_tmp = msg;
-}
+public:
+
+	tracker(std::string pose_topic, std::string color, int frequency)
+	{
+		ros::Subscriber sub = nh_.subscribe(pose_topic, 10, &tracker::pose_callback, this);
+		ros::Publisher marker_pub = nh_.advertise<visualization_msgs::Marker>("tracking_marker", 20);
+		ros::Rate r(frequency);
+
+		visualization_msgs::Marker marker;
+		float* color_rgb[3];
+
+		marker.header.frame_id = "/map";
+		marker.header.stamp = pose_tmp_.header.stamp;
+		marker.ns = "points";
+		marker.action = visualization_msgs::Marker::ADD;
+		marker.pose.orientation.w = 1.0;
+
+		marker.id = 0;
+
+		marker.type = visualization_msgs::Marker::POINTS;
+		marker_scale_ = 0.1;
+		marker.scale.x = marker_scale_;
+		marker.scale.y = marker_scale_;
+		marker.scale.z = marker_scale_;
+
+		// Marker color is sequentially red, orange, yellow, green, cyan, blue, purple.
+		marker.color.a = 1.0;
+		color_rgb = tracker::color_picker(color);
+		marker.color.r = color_rgb[0];
+		marker.color.g = color_rgb[1];	
+		marker.color.b = color_rgb[2];
+
+		while(ros::ok())
+		{
+			geometry_msgs::Point p;
+			p.x = pose_tmp_.pose.pose.position.x;
+			p.y = pose_tmp_.pose.pose.position.y;
+			p.z = pose_tmp_.pose.pose.position.z;
+			marker.points.push_back(p);
+			marker_pub.publish(marker);
+			r.sleep();
+		}
+	}
+
+	void pose_callback(const nav_msgs::Odometry &msg)
+	{
+		pose_tmp_ = msg;
+	}
+
+	float* color_picker(std::string const& color)
+	{
+		static float rgbmat[7][3] = {
+			{1.0, 0.0, 0.0},
+			{1.0, 0.5, 0.0},
+			{1.0, 1.0, 0.0},
+			{0.0, 1.0, 0.0},
+			{0.0, 1.0, 1.0},
+			{0.0, 0.0, 1.0},
+			{0.5, 0.0, 1.0}
+		};
+
+		int i = 7;
+		if (color == "red") 	i = 1;
+		if (color == "orange") 	i = 2;
+		if (color == "yellow") 	i = 3;
+		if (color == "green") 	i = 4;
+		if (color == "cyan") 	i = 5;
+		if (color == "blue") 	i = 6;
+		if (color == "purple") 	i = 7;
+
+		return rgbmat[i-1];
+	}
+
+private:
+	ros::NodeHandle nh_;
+	ros::Subscriber pose_sub;
+	ros::Publisher tracker_pub;
+
+	std::string pose_topic;
+	nav_msgs::Odometry pose_tmp_;
+	float marker_scale_;
+};
+
+
 
 int main(int argc, char** argv)
 {
+	// Number of traces to plot.
+	int count_max = 7;
+	int count = 0;
+	int frequency = 30;
+	std::stringstream ss_tmp;
+	std::string pose_topic_tmp;
+	std::string pose_topic;
+	std::string color_tmp;
+	std::string color;
+	int i;
+
 	ros::init(argc, argv, "tracking_marker_pub");
-	ros::NodeHandle nh;
 
-	ros::Subscriber sub = nh.subscribe(POSE_TOPIC, 10, &pose_callback);
-	ros::Publisher marker_pub = nh.advertise<visualization_msgs::Marker>("tracking_marker", 20);
-	ros::Rate r(20);
-	ros::spinOnce();
+	ros::param::get("trace_amount", count);
+	ros::param::get("frequency", frequency);
 
-	visualization_msgs::Marker marker;
-	marker.header.frame_id = MAP_FRAME_NAME;
-	marker.header.stamp = ros::Time::now();
-	marker.ns = "points";
-	marker.action = visualization_msgs::Marker::ADD;
-	marker.pose.orientation.w = 1.0;
 
-	marker.id = 0;
-
-	marker.type = visualization_msgs::Marker::POINTS;
-	marker.scale.x = 0.07;
-	marker.scale.y = 0.07;
-	marker.scale.z = 0.07;
-
-	// Marker color is red.
-	marker.color.r = 1.0;
-	marker.color.a = 1.0;
-
-	while(ros::ok())
-	{
-		geometry_msgs::Point p;
-		p.x = pose_tmp.pose.pose.position.x;
-		p.y = pose_tmp.pose.pose.position.y;
-		p.z = pose_tmp.pose.pose.position.z;
-
-		marker.points.push_back(p);
-
-		marker_pub.publish(marker);
-
-		ros::spinOnce();
-		r.sleep();
-	}
+	// count trackerd topic numbers
+	for (i = 0; i < count; i++)
+	{	
+		ss_tmp << "pose_topic" << i;
+		pose_topic_tmp = ss_tmp.str();
+		ss_tmp << "color" << i ;
+		color_tmp = ss_tmp.str();
+		ros::param::get(pose_topic_tmp, pose_topic);float
+		ros::param::get(color_tmp, color);
+		// Necessary to change to different names for different instances?
+		tracker tracker(pose_topic, color, frequency);
+	}	
 	
+	ros::spin();
 	return 0;
 }
