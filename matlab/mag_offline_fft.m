481b75deed9022bdf95dbe26c07cc03ebffe6ea2
Tianze Li <tianze.li.eu@gmail.com>
2020-08-25 17:50:22 +0200

    Multiple tracker working now. And added mag matlab file.

--- matlab/mag_offline_fft.m
+++ matlab/mag_offline_fft.m
@@ -4,30 +4,17 @@
 % rostopic echo -b <bagname>.bag -p <topic> > <csvname>.csv
 
 
-IMU_right_complex_test = readtable('imu_right_complex_with_vision.csv', 'PreserveVariableNames', true);
+mag_simpleloop_test = readtable('mag_left_simple_with_vision.csv', 'PreserveVariableNames', true);
 % T0 = ??;           % Bag time length
-IMU_simpleloop_test = readtable('simple_imu_left_bagdata.csv', 'PreserveVariableNames', true);
-% T0 = 389;           % Bag time length
-IMU_static = readtable('imu_left_static.csv', 'PreserveVariableNames', true);
-% T0 = 20;
-IMU_smooth_trans = readtable('imu_left_smooth_tran.csv', 'PreserveVariableNames', true);
-% T0 = 47;
-IMU_smooth_rotate = readtable('imu_left_smooth_rotate.csv', 'PreserveVariableNames', true);
-% T0 = 57;
-IMU_simulated_navi = readtable('imu_left_rough.csv', 'PreserveVariableNames', true);
-% T0 = 40;
-IMU_xshake = readtable('imu_left_xshake.csv', 'PreserveVariableNames', true);
-% T0 = 15;
 
 % Choose what data to process
-IMU_data = IMU_smooth_trans;
-Acc = [IMU_data{:,30}, IMU_data{:,31}, IMU_data{:,32}];
-% For Gyro
-% Acc = [IMU_data{:,18}, IMU_data{:,19}, IMU_data{:,20}];
+mag_data = mag_simpleloop_test;
+Mag = [mag_data{:,5}, mag_data{:,6}, mag_data{:,7}];
+
 
 Fs = 252;                   % Sampling frequency, average for IMU                    
 T = 1/Fs;                   % Sampling period
-[L,~] = size(IMU_data);     % Length of signal
+[L,~] = size(mag_data);     % Length of signal
 t = (0:L-1)*T;              % Time vector
 
 
@@ -45,11 +32,11 @@ t = (0:L-1)*T;              % Time vector
 fc = 3;
 Wn = (2/Fs)*fc;
 b = fir1(20,Wn,'low',kaiser(21,3));
-Acc_lowpass = filter(b,1,Acc);
+Mag_lowpass = filter(b,1,Mag);
 
 % FFT 
-Y = fft(Acc);
-Y_lowpass = fft(Acc_lowpass);
+Y = fft(Mag);
+Y_lowpass = fft(Mag_lowpass);
 
 % To elimate frequency around f-1 < k*16.82 < f + 1 
 d1 = designfilt('bandstopiir','FilterOrder',2, ...
@@ -61,7 +48,7 @@ d2 = designfilt('bandstopiir','FilterOrder',2, ...
 d3 = designfilt('bandstopiir','FilterOrder',2, ...
                'HalfPowerFrequency1',49.6,'HalfPowerFrequency2',51.6, ...
                'DesignMethod','butter','SampleRate',Fs);
-buttLoop1 = filtfilt(d1,Acc_lowpass);
+buttLoop1 = filtfilt(d1,Mag_lowpass);
 buttLoop2 = filtfilt(d2,buttLoop1);
 buttLoop = filtfilt(d3,buttLoop2);
 Y_bandkill = fft(buttLoop);
@@ -69,13 +56,13 @@ Y_bandkill = fft(buttLoop);
 figure;
 for i=1:3
     subplot(3,2,2*i-1)
-    plot(t,Acc(:,i));
+    plot(t,Mag(:,i));
     grid on;
-    title(['Acc ',num2str(i),' in the Time Domain'])
+    title(['Mag ',num2str(i),' in the Time Domain'])
     subplot(3,2,2*i)
     plot(t,buttLoop(:,i))
     grid on;
-    title(['Acc Filtered ',num2str(i),' in the Time Domain'])
+    title(['Mag Filtered ',num2str(i),' in the Time Domain'])
 end
 
            
@@ -97,9 +84,9 @@ for i=1:3
     f = Fs*(0:(L/2))/L;
     plot(f,P1(:,i));
     grid on;
-    title(['Acc ',num2str(i),' in the Freq-Domain'])
+    title(['Mag ',num2str(i),' in the Freq-Domain'])
     subplot(3,2,2*i)
     plot(f,P1_pass(:,i));
     grid on;
-    title(['Acc low-passed ',num2str(i),' in the Freq-Domain'])
+    title(['Mag low-passed ',num2str(i),' in the Freq-Domain'])
 end
