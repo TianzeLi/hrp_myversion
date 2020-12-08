690bb874b010ae12534ad1554a5eed5afb3b4325
Tianze Li <tianze.li.eu@gmail.com>
2020-12-08 16:10:58 +0100

    Complete the fundamental boundary detect pipeline for p3_color.

--- am_vision/script/boundary_detect.py
+++ am_vision/script/boundary_detect.py
@@ -20,11 +20,12 @@ if not src.data:
 cv2.imshow('Source Image', src)
 cv2.waitKey(0)
 
-# Display the edges detected by Canny operator.
-edges = cv2.Canny(src,100,200)
-cv2.imshow('Canny Edge on Source Image', edges)
-cv2.waitKey(0)
+# # Display the edges detected by Canny operator.
+# edges = cv2.Canny(src,100,200)
+# cv2.imshow('Canny Edge on Source Image', edges)
+# cv2.waitKey(0)
 
+# Blur the image.
 # src_blur = cv2.blur(src,(6,6))
 # # src_blur = cv2.GaussianBlur(src,(5,5),0)
 # # src_blur = cv2.bilateralFilter(src,9,75,75)
@@ -72,6 +73,64 @@ img_color_threshold = cv2.bitwise_and(src, src, mask = mask_green)
 cv2.imshow("Within color threshold", img_color_threshold)
 cv2.waitKey(0)
 
+# Two threshold values, minVal and maxVal. 
+# Any edges with intensity gradient more than maxVal are sure to be edges 
+# and those below minVal are sure to be non-edges, so discarded. 
+# Those who lie between these two thresholds are classified edges or 
+# non-edges based on their connectivity. 
+# If they are connected to sure-edge pixels, they are considered to be part of edges. 
+# Otherwise, they are also discarded. 
+edges = cv2.Canny(mask_green, 100, 200, L2gradient=True)
+edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, kernel4)
+_, contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+# contours = cv2.findContours(mask_total.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2]
+
+cv2.imshow('Canny Edge on Threshold Image', edges)
+cv2.waitKey(0)
+
+# Draw the contours before area check.
+src_contour = cv2.drawContours(src.copy(), contours, -1, (0,255,0), 1)
+cv2.imshow("Contours", src_contour)
+cv2.waitKey(0)
+
+# contour area restriction
+# large_area = 2500
+area_min = 900
+length_min = 500
+
+# Create the queue to contain the selected contours.
+lawn_contour = []
+
+if len(contours) > 0:
+	i = -1
+	for cnt in contours:
+		i += 1
+		area = cv2.contourArea(cnt)
+		length = cv2.arcLength(cnt,True)
+
+		if (area < area_min):
+			continue
+
+		if length < length_min:
+			continue
+
+		print("Got a contour!")
+		lawn_contour.append(cnt)
+		print('The area is %.3f' % area )
+		print('The length is %.3f' % length)
+		# Print the first child contour and the parent contour
+		# print(hierarchy[0][i][2])
+		if hierarchy[0][i][3] > 0:
+			print('Has the parent contour no. %d' % hierarchy[0][i][3])
+		print('\n')
+else:
+	print('No contour fouond before examining the shapes.')
+
+# Draw all the passed contours
+src_contour_dst = cv2.drawContours(src, lawn_contour, -1, (0,255,255), 5)
+# Draw the contours after the area check.
+cv2.imshow("Lawn Contours", src_contour_dst)
+cv2.waitKey(0)
 
 # cv2.bitwise_not(src, img_color_filtered, mask_green);
 
