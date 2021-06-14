d02e1a000050d1b38fb03790c9e70b552d0fdeed
Tianze Li <tianze.li.eu@gmail.com>
2021-06-14 16:08:38 +0200

    Add scripts to unify the timestamps in bags and add gnss_process for the original GPS.

---
+++ test/scripts/gnss_process.py
@@ -0,0 +1,71 @@
+#! /usr/bin/python3
+
+"""
+ Refine embedded origin GPS measurement for fusion.
+
+"""
+
+import rospy
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from sensor_msgs.msg import NavSatFix
+
+
+class GNSSProcess():
+
+	def __init__(self):
+
+		topic_name = "/GPSfix"
+		self.x_scale_factor = 0.5
+		self.y_scale_factor = 1.0
+		self.do_estimate_initial = True
+		
+		self.degree2meter = 111320
+		self.initial_latitude = 59.40455 
+		self.initial_longitude = 17.94965
+		self.latitude_stdev = 5.0
+		self.longitude_stdev = 5.0
+
+		if (self.do_estimate_initial):
+			self.initial_latitude = 0.0 
+			self.initial_longitude = 0.0
+			self.initial_stack_number = 3
+			self.number_count = 0
+
+		self.pose_pub = rospy.Publisher((topic_name + "_processed"), PoseWithCovarianceStamped, queue_size=10)
+		rospy.Subscriber(topic_name, NavSatFix, self.gpsfix_callback)
+
+	def gpsfix_callback(self, msg):
+		to_pub = PoseWithCovarianceStamped()
+		to_pub.header = msg.header
+		to_pub.header.stamp = rospy.Time.now()
+
+		if (self.do_estimate_initial):
+			if (self.initial_stack_number > self.number_count):
+				self.initial_latitude += msg.latitude
+				self.initial_longitude += msg.longitude
+				self.number_count += 1
+			else:
+				self.initial_latitude = self.initial_latitude / self.initial_stack_number
+				self.initial_longitude = self.initial_longitude / self.initial_stack_number
+				self.do_estimate_initial = False
+
+		if not self.do_estimate_initial:
+			x,y = self.compute_local_corrdinate(msg.latitude, msg.longitude)
+			to_pub.pose.pose.position.x = x * self.x_scale_factor
+			to_pub.pose.pose.position.y = y * self.y_scale_factor
+			# to_pub.pose.pose.covariance[0] = msg.position_covariance[0]
+			# to_pub.pose.pose.covariance[9] = msg.position_covariance[4]
+			to_pub.pose.covariance[0] = self.longitude_stdev
+			to_pub.pose.covariance[9] = self.latitude_stdev
+			self.pose_pub.publish(to_pub)
+
+	def compute_local_corrdinate(self, lati, longi):
+		x_local = self.degree2meter*(longi - self.initial_longitude)
+		y_local = self.degree2meter*(lati - self.initial_latitude)
+		return x_local, y_local
+
+
+if __name__ == '__main__':
+	rospy.init_node('gnss_simu')
+	GNSSProcess()
+	rospy.spin()
---
+++ test/scripts/rewrite_bag_stamp.py
@@ -0,0 +1,80 @@
+#! /usr/bin/python3
+
+"""
+ Rewrite the timestamp of certain topics in the rosbag. 
+ In particular, the timestamp can be unified when recorded on distributed
+ devices that offer different time stamps. 
+
+ t is the time the the message is received, while header.stamp can contain the 
+ time when the message is generated.
+
+"""
+
+import rosbag 
+import rospy
+from os.path import expanduser
+import math
+import yaml
+import sys
+import subprocess
+import time
+
+
+
+def status(length, percent):
+    sys.stdout.write('\x1B[2K') # Erase entire current line
+    sys.stdout.write('\x1B[0E') # Move to the beginning of the current line
+    progress = "Progress: ["
+    for i in range(0, length):
+        if i < length * percent:
+            progress += '='
+        else:
+            progress += ' '
+    progress += "] " + str(round(percent * 100.0, 2)) + "%"
+    sys.stdout.write(progress)
+    sys.stdout.flush()
+
+
+#  Bag location and read.
+home = expanduser("~")
+dir_name = home + "/bags/lawn/"
+filename = "lawn_one_lap_20201122"
+orig_bag = rosbag.Bag(dir_name + filename + ".bag")
+restamped_path = dir_name + filename + "_restamped.bag"
+# Threshold that if larger, consider as the lagged time stamp
+max_offset = 5.0
+
+
+info_dict = yaml.load(subprocess.Popen(['rosbag', 'info', '--yaml', 
+                                        (dir_name + filename + ".bag")], 
+                                        stdout=subprocess.PIPE).communicate()[0],
+                                        Loader=yaml.FullLoader)
+                                        
+duration = info_dict['duration']
+start_time = info_dict['start']
+
+
+with rosbag.Bag(restamped_path, 'w') as outbag:
+
+    last_time = time.clock()
+    for topic, msg, t in orig_bag.read_messages():
+        if (time.clock() - last_time > .1) and (t.to_sec() > start_time):
+            percent = (t.to_sec() - start_time) / duration
+            status(40, percent)
+            last_time = time.clock()
+
+        if topic == "/tf" and msg.transforms:
+          # Writing transforms to bag file 1 second ahead of time to ensure availability
+            for transform in msg.transforms:
+                if t.to_sec() - transform.header.stamp.to_sec() > 100.0:
+                    transform.header.stamp = t
+            outbag.write(topic, msg, t)
+        elif msg._has_header:
+            if t.to_sec() - msg.header.stamp.to_sec() > 100.0:
+                msg.header.stamp = t
+            outbag.write(topic, msg, t)
+        else:
+            outbag.write(topic, msg, t)
+
+status(40, 1)
+print("\nDone.")
